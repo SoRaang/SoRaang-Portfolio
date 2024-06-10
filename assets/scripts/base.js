59b407aebb808324eb9eabadd52ad31ee498550c
SoRaang <dsc0320@hotmail.com>
2024-06-10 19:19:17 +0900

    Patch 0
    Added:
    Portfolio section, project data, etc.

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const contPortfolio = document.getElementById('contPortfolio');
+const portfolioData = await fetch('/assets/scripts/projectData.json').then(data => data.json());
+
+console.log(contPortfolio);
+console.log(portfolioData);
+
+class makePortfolio {
+    constructor(projectData, targetDOM) {
+        this.data = projectData;
+        this.target = targetDOM;
+    }
+
+    init() { /** 기초 컨테이너 생성 */
+
+    }
+
+    printData() { /** 컨테이너에 데이터 객체별 DOM 출력 */
+        this.data.forEach((dataRow) => {
+            const divMan = document.createElement('article');
+
+            divMan.classList.add('pf-totalyear-container');
+        });
+    }
+
+    showDetails() { /** 프로젝트 선택시 상세 내용 모달 표시 */
+
+    }
+}
